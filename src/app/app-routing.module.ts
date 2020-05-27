843ad8c753f1fd030c3704781b1fb19e880f1f70
Koenable <mokoenabaholo90@gmail.com>
2020-05-27 05:07:52 +0200

    First Stable Custom Scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { OurcompanyComponent } from './ourcompany/ourcompany.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, 
+  { path: "", redirectTo: "/home", pathMatch: "full" },
   { path: 'home', component: HomeComponent }, 
   { path: 'ourcompany', component: OurcompanyComponent },  
 
