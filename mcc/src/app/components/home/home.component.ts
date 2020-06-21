1aa4930bbcf80bb9b052e403eb2f5b0cbfc74fab
Koena <mokoenabaholo90@gmail.com>
2020-06-22 01:59:37 +0200

    added white background on .view class in the home component

--- mcc/src/app/components/home/home.component.ts
+++ mcc/src/app/components/home/home.component.ts
@@ -36,7 +36,7 @@ import Glide from '@glidejs/glide'
     trigger('slideinLeft', [
       transition('void => *', [
           style({opacity:0, transform: 'translateX(100%)'}),
-          animate('0.6s 1000ms ease-in-out')
+          animate('0.9s 3000ms ease-in-out')
       ]),
       transition('* => void', [
           animate('10ms ease-in-out', style({transform: 'translateY(100%)'}))    
