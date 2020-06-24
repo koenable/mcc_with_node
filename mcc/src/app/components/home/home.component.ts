9e09f205164837dbd4bff47fcfb54b1c57d00a40
Baholo <Mokoenabaholo90@gmail.com>
2020-06-24 13:20:35 +0200

    Create new mixens. Needs to remane then on the HTML.

--- mcc/src/app/components/home/home.component.ts
+++ mcc/src/app/components/home/home.component.ts
@@ -1,67 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 // dependencies for animations
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
+import { slideDownFirst, slideUp,slideInRight } from "./../../shared/animations";
 // import slider 
-import Glide from '@glidejs/glide'
+// import Glide from '@glidejs/glide'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [
-    trigger('slideDown', [
-        transition('void => *', [
-            style({opacity:0, transform: 'translateY(-10%)'}),
-            animate('0.6s 2000ms ease-in-out')
-        ]),
-        transition('* => void', [
-            animate('10ms ease-in-out', style({transform: 'translateY(100%)'}))    
-        ])
-    ]),
-    trigger('slideUp', [
-      transition('void => *', [
-          style({opacity:0, transform: 'translateY(10%)'}),
-          animate('0.6s 3000ms ease-in-out')
-      ]),
-      transition('* => void', [
-          animate('10ms ease-in-out', style({transform: 'translateY(100%)'}))    
-      ])
-  ]),
-    trigger('slideinLeft', [
-      transition('void => *', [
-          style({opacity:0, transform: 'translateX(100%)'}),
-          animate('0.9s 3000ms ease-in-out')
-      ]),
-      transition('* => void', [
-          animate('10ms ease-in-out', style({transform: 'translateY(100%)'}))    
-      ])
-  ]),
-]
+  animations: [ slideDownFirst, slideUp,slideInRight]
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-    new Glide('.glide', {
-      type: 'carousel',
-      autoplay: 2000,
-      gap:1,
-      animationDuration: 1000,
-      animationTimingFunc: 'linear',
-      perView: 3,
-      peek: 300,
-      breakpoints: {
-        879: {perView:2},
-        630:{perView:1}
-      }
-    }).mount()
+    // new Glide('.glide', {
+    //   type: 'carousel',
+    //   autoplay: 2000,
+    //   gap:1,
+    //   animationDuration: 1000,
+    //   animationTimingFunc: 'linear',
+    //   perView: 3,
+    //   peek: 300,
+    //   breakpoints: {
+    //     879: {perView:2},
+    //     630:{perView:1}
+    //   }
+    // }).mount()
     
   }
 
--- mcc/src/app/components/topnav/topnav.component.ts
+++ mcc/src/app/components/topnav/topnav.component.ts
@@ -1,33 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-// dependencies for animations
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
-
 // import gsap to create menu overlay animation
+import { Component, OnInit } from '@angular/core';
 import { gsap } from "gsap";
-
+// import aniations
+import { slideDownAfter } from "./../../shared/animations";
 
 
 @Component({
   selector: 'app-topnav',
   templateUrl: './topnav.component.html',
   styleUrls: ['./topnav.component.scss'],
-  animations: [
-    trigger('slideDown', [
-      transition('void => *', [
-        style({ opacity: 0, transform: 'translateY(-10%)' }),
-        animate('0.6s 3000ms ease-in-out')
-      ]),
-      transition('* => void', [
-        animate('10ms ease-in-out', style({ transform: 'translateY(100%)' }))
-      ])
-    ])
-  ]
+  animations: [ slideDownAfter]
 })
 export class TopnavComponent implements OnInit {
 
