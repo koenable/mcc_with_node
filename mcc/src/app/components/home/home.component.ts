da96f58ca1e39f6cac5c9b6eaed557af3b186e15
Koena <mokoenabaholo90@gmail.com>
2020-06-26 23:12:37 +0200

    Glide js has been implemented in the content section of the landing page. Its still needs to work to make it refully responsive

--- mcc/src/app/components/home/home.component.ts
+++ mcc/src/app/components/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 // dependencies for animations
 import { slideDownFirst, slideUp,slideInRight, slideDownAfter } from "./../../shared/animations";
 // import slider 
-// import Glide from '@glidejs/glide'
+import Glide from '@glidejs/glide'
+
 
 @Component({
   selector: 'app-home',
@@ -15,20 +16,59 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // new Glide('.glide', {
-    //   type: 'carousel',
-    //   autoplay: 2000,
-    //   gap:1,
-    //   animationDuration: 1000,
-    //   animationTimingFunc: 'linear',
-    //   perView: 3,
-    //   peek: 300,
-    //   breakpoints: {
-    //     879: {perView:2},
-    //     630:{perView:1}
-    //   }
-    // }).mount()
-    
+
+//https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.0.2/glide.js
+
+// var glideMulti = new Glide('.multi', {
+//   type: 'slider',
+//   autoplay: 4000,
+//   focusAt: 'center',
+//   perView: 2
+// });
+
+var glideMulti1 = new Glide('.multi1', {
+  // type: 'carousel',
+  type: 'slider',
+  autoplay: 5000,
+  perView: 3,
+  animationDuration: 1000,
+ 
+  // gap:
+});
+
+// var glideHeroPeek = new Glide('.heropeek', {
+//   type: 'carousel',
+//   animationDuration: 1000,
+//   autoplay: 3000,
+//   focusAt: '1',
+//   startAt: 1,
+//   perView: 1,
+//    // set a value to show the previous and next slides peeking in
+//   peek: {
+//     before: 20,
+//     after: 20
+//   },
+//   g  
+// });
+
+// var glideHero = new Glide('.hero', {
+//   type: 'carousel',
+//   animationDuration: 2000,
+//   autoplay: 4500,
+//   focusAt: '1',
+//   startAt: 3,
+//   perView: 1, 
+// });
+
+// glideMulti.mount();
+glideMulti1.mount();
+// glideHeroPeek.mount();
+// glideHero.mount();
+
+
+
+
+      
   }
 
  
