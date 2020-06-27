99be48d007559771d453df78b2933abc7cda82f8
Koena <mokoenabaholo90@gmail.com>
2020-06-27 02:46:59 +0200

    Parner's slide complete. Added social media href destination address to icons on the navbar

--- mcc/src/app/components/home/home.component.ts
+++ mcc/src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // dependencies for animations
-import { slideDownFirst, slideUp,slideInRight, slideDownAfter } from "./../../shared/animations";
+import { slideDownFirst, slideUp, slideInRight, slideDownAfter } from "./../../shared/animations";
 // import slider 
 import Glide from '@glidejs/glide'
 
@@ -10,7 +10,7 @@ import { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [ slideDownFirst, slideUp,slideInRight, slideDownAfter]
+  animations: [slideDownFirst, slideUp, slideInRight, slideDownAfter]
 })
 export class HomeComponent implements OnInit {
 
@@ -18,66 +18,36 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
 
-//https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.0.2/glide.js
+    var glideMulti1 = new Glide('.glide', {
+      // type: 'carousel',
+      type: 'slider',
+      autoplay: 3000,
+      perView: 4,
+      gap: 0,
+      animationDuration: 1000,
+      peek: {
+        before: 4,
+        after: 1
+      },
+      breakpoints: {
+        992: {
+          perView: 1,
+          autoplay: 2000
+        }
 
-// var glideMulti = new Glide('.multi', {
-//   type: 'slider',
-//   autoplay: 4000,
-//   focusAt: 'center',
-//   perView: 2
-// });
+      },
 
-var glideMulti1 = new Glide('.multi1', {
-  // type: 'carousel',
-  type: 'slider',
-  // autoplay: 5000,
-  perView: 4,
-  gap:0,
-  animationDuration: 1000,
-  breakpoints: {
-    576: {
-      perView:1,
-      autoplay:2000
-    }
-  }
-  // gap:
-});
-
-// var glideHeroPeek = new Glide('.heropeek', {
-//   type: 'carousel',
-//   animationDuration: 1000,
-//   autoplay: 3000,
-//   focusAt: '1',
-//   startAt: 1,
-//   perView: 1,
-//    // set a value to show the previous and next slides peeking in
-//   peek: {
-//     before: 20,
-//     after: 20
-//   },
-//   g  
-// });
-
-// var glideHero = new Glide('.hero', {
-//   type: 'carousel',
-//   animationDuration: 2000,
-//   autoplay: 4500,
-//   focusAt: '1',
-//   startAt: 3,
-//   perView: 1, 
-// });
-
-// glideMulti.mount();
-glideMulti1.mount({ Controls, Breakpoints });
-// glideHeroPeek.mount();
-// glideHero.mount();
+    })
+      .mount({ Controls, Breakpoints });
 
 
 
 
-      
+
+
   }
 
- 
+
+
 
 }
